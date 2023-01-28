98a20d813cf9370c8732c9e01948cda9fe36b6b5
Gautam <76781773+Genstark@users.noreply.github.com>
2023-01-28 17:43:20 +0530

    Update audio_to_image.py

--- audio_to_image.py
+++ audio_to_image.py
@@ -4,7 +4,7 @@ def spectogram_librosa(wav_file):
     import numpy as np
 
     (sig, rate) = librosa.load(wav_file, sr=32000, mono=True,  duration=1, dtype=np.float32)
-    name = pylab.specgram(sig, Fs=rate)
+    pylab.specgram(sig, Fs=rate)
     pylab.savefig('spectrogram.jpg')
 
 spectogram_librosa("car.wav")
