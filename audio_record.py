446baf749453ec0e32b6f5e599a9e6ab04239c5b
Gautam <76781773+Genstark@users.noreply.github.com>
2023-02-04 12:16:27 +0530

    Update audio_record.py

--- audio_record.py
+++ audio_record.py
@@ -7,5 +7,4 @@ seconds = 2  # Duration of recording
 myrecording = sd.rec(int(fs * seconds), samplerate=fs, channels=1)
 sd.wait()  # Wait until recording is finished
 
-# Save the recording to a WAV file
 write("recorded.wav", fs, myrecording)
