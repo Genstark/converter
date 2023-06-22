144808fb457b6196c55ee3c385d13acc702a603c
Gautam <76781773+Genstark@users.noreply.github.com>
2023-06-22 14:08:51 +0530

    pbtxt label conveter

---
+++ labelmap.py
@@ -0,0 +1,18 @@
+classnames_file = "dataset/class-name.txt"
+protobuf_file = "dataset/labelmap.pbtxt"
+
+file  = open(classnames_file,'r')
+output_dict ={}
+classname = file.readline().strip()
+count=1
+while len(classname) > 0:
+    output_dict[classname] = count
+    classname = file.readline().strip()
+    count+=1
+file.close()
+
+outfile = open(protobuf_file,'w+')
+outfile.truncate(0)
+for i in output_dict.keys():   
+    outfile.write("item {\n"+"  id:"+ str(output_dict[i]) + '\n'+'  name:'+"'" +str(i)+"'" +'\n'+ "}\n")
+outfile.close() 
